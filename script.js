22aed97d4a815c89d242ee6371520e511a579cdd
lee454ho <lee454ho@gmail.com>
2022-07-20 17:22:46 +0900

    first commit

--- script.js
+++ script.js
@@ -55,6 +55,86 @@ footerScroll.addEventListener('click',function(e){
     }
 })
 ///////////////////////////////////////
+
+//슬라이드이벤트 셀렉터
+let btnR = document.querySelector("#btnR");
+let btnL = document.querySelector("#btnL");
+let item = document.querySelectorAll(".slideItem")
+let slider = document.querySelector(".bestSeller-sell-move")
+
+let itemlength =item.length
+let itemIndex = 0
+
+let firstChild = slider.firstElementChild;
+let firstclone = firstChild.cloneNode(true);
+
+console.log(slider)
+
+
+btnL.addEventListener("click",function(){
+    if(itemIndex <= item.length){
+        slider.style.transform = "translate(" + 100 * (itemIndex + 1)+"px)"
+    }
+    console.log(itemIndex)
+    item[++itemIndex]
+    
+    if(itemIndex === item.length -1){
+    setTimeout(function(){
+        
+            slider.style.transition = "0ms"
+            slider.style.transform = "translate(" + 100 * (itemIndex + 1)+"px)"
+        },300);
+        itemIndex = -1;
+    }
+    item[++itemIndex]
+        
+    
+    
+})
+btnR.addEventListener("click",function(){
+    if(itemIndex <= item.length - 1){
+        slider.style.transform = "translate(-" + 100 * (itemIndex + 1)+"px)"
+    }
+    item[++itemIndex]
+})
+
+
+//네비바 픽스트 이벤트
+let scrollNav_bar = document.querySelector(".nav-bar"); 
+
+window.addEventListener("scroll",function(){
+    console.log(window.pageYOffset)
+    if(window.pageYOffset > 30){
+        scrollNav_bar.className = "nav-bar-fixed"
+    }
+    else if(window.pageYOffset < 30){
+        scrollNav_bar.className = "nav-bar"
+    }
+
+    
+})
+
+
+//네비바 스크롤 이동 이벤트 
+let scrollele = document.querySelectorAll(".scroll-ele");
+let scrollpotint = document.querySelectorAll(".scroll");
+let first = scrollpotint[0].offsetTop; 
+let second = scrollpotint[1].offsetTop;
+let third = scrollpotint[2].offsetTop;
+console.log(scrollele[1])
+
+scrollele[0].addEventListener("click",function(){
+    window.scroll({top:first,behavior:"smooth"});
+})
+scrollele[1].addEventListener("click",function(){
+    window.scroll({top:second,behavior:"smooth"});
+})
+scrollele[2].addEventListener("click",function(){
+    window.scroll({top:third,behavior:"smooth"});
+})
+
+
+
 //////////////////상호//////////////////
                                      
 ///////////////////////////////////////
